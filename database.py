e0f64d1ce83dba1c055d5e392787134d84af3dd3
Sajibrasel <your-email@example.com>
2026-08-31 14:45:46 +0600

    Fix username of admin ID 8904339611 to None

--- database.py
+++ database.py
@@ -121,7 +121,7 @@ async def init_db() -> None:
             await cur.execute(
                 "INSERT INTO bot_admins (user_id, username, first_name) VALUES (%s, %s, %s) "
                 "ON DUPLICATE KEY UPDATE username=VALUES(username), first_name=VALUES(first_name)",
-                (8904339611, "nikitaa92", "নিকিতা জাহান")
+                (8904339611, None, "নিকিতা জাহান")
             )
 
 
